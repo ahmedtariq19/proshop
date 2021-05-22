2b460a43ddba7a0c7cf26bd26aa64e90ccfad1db
Ahmed <Admin@DESKTOP-ET02UF8.local>
2021-05-22 21:56:44 +0500

    Admin part 1

--- backend/middleWare/authMiddleware.js
+++ backend/middleWare/authMiddleware.js
@@ -22,3 +22,11 @@ export const protect = asyncHandler(async (req, res, next) => {
     throw new Error("Unauthorized access, no token");
   }
 });
+export const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Unauthorized as an admin");
+  }
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,16 +1,25 @@
 import express from "express";
-import { protect } from "../middleWare/authMiddleware.js";
+import { protect, admin } from "../middleWare/authMiddleware.js";
 const router = express.Router();
 import {
   userAuth,
   registerUser,
   getUserProfile,
   updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
 } from "../Controllers/userControllers.js";
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/login", userAuth);
 router
   .route("/profile")
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 export default router;
